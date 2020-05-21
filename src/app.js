579a4b0c2e4754831598c30f2faa994a41a26013
Augusto Cezar <augusto.cezar@rockcontent.com>
2020-05-20 23:27:54 -0300

    Project CRUD & Likes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,80 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  
+  response.json(repositories);
+
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title,techs,url} = request.body;
+
+  const repository = {
+    id : uuid(),
+    title,
+    url,
+    techs,
+    likes : 0
+  }
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {title,techs,url} = request.body;
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => 
+   repository.id === id );
+
+  if(repositoryIndex< 0){
+    return response.statusCode(400).send();
+  }
+    
+   const repository = {
+     id,
+     title,
+     url,
+     techs,
+     likes: repositories[repositoryIndex].likes
+   };
+
+   repositories[repositoryIndex]= repository;
+
+   return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+ const { id } = request.params;
+ 
+ const repositoryIndex = repositories.findIndex(repository => repository.id === id );
+
+  if(repositoryIndex >= 0){
+    repositories.splice(repositoryIndex,1);
+  }else{
+    return response.statusCode(404).send();
+ }
+
+  return response.statusCode(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id );
+
+
+   if(repositoryIndex < 0){
+    return response.statusCode(400).send();
+  }
+
+  repositories[repositoryIndex].likes++;
+
+  return response.json(repositories[repositoryIndex]);
+
 });
 
 module.exports = app;
